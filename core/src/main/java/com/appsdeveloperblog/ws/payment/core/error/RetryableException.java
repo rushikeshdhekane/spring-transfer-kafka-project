bb05f1b04ddf035671b82ed4aa721543eac01bf2
Rushikesh <dhekanerushikesh@gmail.com>
2024-12-09 18:57:16 +0530

    core shared obeject

---
+++ core/src/main/java/com/appsdeveloperblog/ws/payment/core/error/RetryableException.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.ws.payment.core.error;
+
+public class RetryableException extends RuntimeException {
+    public RetryableException(Throwable cause) {
+        super(cause);
+    }
+
+    public RetryableException(String message) {
+        super(message);
+    }
+}
